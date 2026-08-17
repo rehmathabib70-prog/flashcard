64158ca44abea12e89e15a62ee317346041dbb52
rehmathabib70-prog <rehmathabib70@gmail.com>
2026-08-18 01:17:37 +0500

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,10 @@
+{
+    "name": "English Vocabulary Flash Cards",
+    "short_name": "English Cards",
+    "start_url": ".",
+    "display": "standalone",
+    "background_color": "#f3f4f6",
+    "theme_color": "#111827",
+    "description": "English vocabulary flash cards with swipe learning and revision.",
+    "icons": []
+}
